2349377c4ecf73e1d2431de361f7e30888a3191a
Gbeshk <giorgibeshkenadze4@gmail.com>
2024-10-21 06:24:12 +0400

    adwdaw

---
+++ script.js
@@ -0,0 +1,33 @@
+let x = 9;
+let y = x * x;
+if (y > 100) {
+  console.log("მეტია ასზე");
+} else if (y < 100) {
+  console.log("ნაკლებია ასზე");
+} else {
+  console.log("უდრის ასს");
+}
+
+let ricxvi = 77;
+if (ricxvi > 50 && ricxvi < 100){
+    console.log("არის 50_ზე მეტი და 100_ზე ნაკლები")
+} else {
+    console.log("არ არის 50_ზე მეტი და 100_ზე ნაკლები")
+}
+
+let a=2;
+if (a<=0){
+    console.log("0_ის ტოლია ან უარყოფითია")
+}else {
+    console.log("არ არის 0_ის ტოლია ან უარყოფითი")
+}
+
+let sigrdze = 10;
+let sigane = 5;
+let p=2*(sigane + sigrdze);
+if (p>50){
+    console.log("პერიმეტრი 50_ზე მეტია")
+}else {
+    console.log("პერიმეტრი 50_ზე მეტი არ არის")
+}
+
